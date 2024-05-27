77fb3bd5e9d865153f63157cb50403ddb236994c
Mike <dinclock5@gmail.com>
2024-05-27 16:13:50 +0300

    fix env migration

--- bot/migration/env.py
+++ bot/migration/env.py
@@ -26,7 +26,6 @@ if config.config_file_name is not None:
 target_metadata = Base.metadata
 
 config.set_main_option("sqlalchemy.url", settings.db_url)
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
