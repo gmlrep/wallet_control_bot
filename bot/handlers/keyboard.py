b5270b15fb467fb6deff3550950a6088c9122749
Mike <dinclock5@gmail.com>
2024-06-21 13:20:19 +0300

    edit keyboard

--- bot/handlers/keyboard.py
+++ bot/handlers/keyboard.py
@@ -64,4 +64,3 @@ async def kb_settings(user_id: int):
     settings.button(text="◀️ Назад", callback_data='back')
     settings.adjust(1, 1)
     return settings.as_markup()
-
--- bot/wallet_control.py
+++ bot/wallet_control.py
@@ -88,3 +88,12 @@ async def check_address(address: str):
                 return False
             except KeyError:
                 return True
+
+
+
+import re
+
+str = '3 товара за 200.99'
+pat = r'\d+.\d'
+match = re.search(pat, str)
+print(match.group())
