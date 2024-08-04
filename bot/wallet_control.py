fdf66653adefb6afe2da6838962cb35a6b19cca1
Mike <dinclock5@gmail.com>
2024-08-04 11:31:37 +0300

    update: add docstring

--- bot/wallet_control.py
+++ bot/wallet_control.py
@@ -1,19 +1,27 @@
 import asyncio
 
 import aiohttp
-from typing import Optional
 
 from bot.db.config import settings
 
 
-async def get_inf_api(url: str, headers: dict) -> dict:
+async def get_inf_api(url: str, headers: dict) -> dict | None:
     async with aiohttp.ClientSession() as session:
         async with session.get(url=url, headers=headers) as response:
+            if response.status != 200:
+                return None
             data = await response.json(encoding='utf8')
             return data
 
 
-async def get_balance_jettons(wallet_address: str) -> Optional[dict]:
+async def get_balance_jettons(wallet_address: str) -> dict | None:
+    """
+    Balance of ton and jettons from TON for wallet address
+
+    :param wallet_address: address of wallet on ton blockchain
+    :return: balance ton and jetton :class:`dict`
+    """
+
     api_key = settings.api_token
     urls = {
         'url': f'https://tonapi.io/v2/accounts/{wallet_address}/jettons?currencies=ton,usd',
@@ -33,7 +41,6 @@ async def get_balance_jettons(wallet_address: str) -> Optional[dict]:
         diff_24h_usd = ton_price['rates']['TON']['diff_24h']['USD'].split('%')[0]
         diff_24h_usd = float(diff_24h_usd.replace('−', '-'))
 
-
         native = {
             'jetton_name': 'TON',
             'balance': int(ton_wallet['balance']) / 1000000000,
@@ -48,9 +55,10 @@ async def get_balance_jettons(wallet_address: str) -> Optional[dict]:
         }
         jettons = []
         for resp in jetton_wallet['balances']:
-            if int(resp['balance']) != 0:
-                balance = int(resp['balance']) / 10**int(resp['jetton']['decimals'])
+            if (bal := int(resp['balance'])) != 0:
+                balance = bal / 10**int(resp['jetton']['decimals'])
                 value = balance * resp['price']['prices']['USD']
+
                 if value > 0.1:
                     diff_24h_ton = resp['price']['diff_24h']['TON'].split('%')[0]
                     diff_24h_usd = resp['price']['diff_24h']['USD'].split('%')[0]
@@ -79,7 +87,7 @@ async def get_balance_jettons(wallet_address: str) -> Optional[dict]:
         jettons.sort(key=lambda x: x['value_usd'], reverse=True)
         return {'native': native, 'jettons': jettons}
     except KeyError:
-        pass
+        return
 
 
 async def check_address(address: str) -> bool:
